5a61bab5d3872a88bc501b04adea9148b926daa1
ahmedadhem <ahmedadhem330@gmail.com>
2025-08-07 18:50:47 +0300

    end edit wibsite dashbourd

--- components/layout/dashboard/page.tsx
+++ components/layout/dashboard/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import DashboardPage from './dashboard.client'
+//import DashboardPage from './dashboard.client'
 
 export default function page() {
   return (
     <div className="">
-      <DashboardPage />
     </div>
   )
 }
